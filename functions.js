1982fe5fb1049be671c3e0ff295b2938326612c0
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-07-19 16:52:42 +0100

    drawCalendar() => optimised + comments added

--- functions.js
+++ functions.js
@@ -1,22 +1,17 @@
-let monthDays = document.getElementById("days");
-
-drawCalendar(firstDay, monthLength);
-
 function drawCalendar(firstDay, monthLength) {
-  firstDay == 6 && monthLength == 31 ? (n = 42) : (n = 35);
-  firstDay == 7 && monthLength >= 30 ? (n = 42) : (n = 35);
-  firstDay == 1 && monthLength == 28 ? (n = 28) : (n = 35); //! corregir container border
-
+  //*funcion que pinta el calendario
+  firstDay == 6 && monthLength == 31 ? (n = 42) : (n = 35); // si el dia 1 es Sábado y el mes tiene 31 pinta 42 casillas
+  firstDay == 7 && monthLength >= 30 ? (n = 42) : (n = 35); // si el dia 1 es Dmingo y el mes tiene 30 o 31 pinta 42 casillas
+  firstDay == 1 && monthLength == 28 ? (n = 28) : (n = 35); // si el dia 1 es Lunes y el mes tiene 28 pinta 42 casillas
+  //! en este caso, corregir container border
   for (let i = 1; i <= n; i++) {
-    var square = document.createElement("div");
+    var square = document.createElement("div"); //creamos un div
     if (i >= firstDay && i < firstDay + monthLength) {
-      var str = mm + "-" + (1 + i - firstDay);
-      square.setAttribute("id", str);
-      square.innerHTML = 1 + i - firstDay;
+      //si esta entre el primero del mes y  el primero del mes + dias del mes
+      square.setAttribute("id", mm + "-" + (1 + i - firstDay)); //le añadimos el id para despues manejarlo
+      square.innerHTML = 1 + i - firstDay; //pintamos el número de día
     }
-    square.setAttribute("class", "day-style");
-    monthDays.appendChild(square);
+    square.setAttribute("class", "day-style"); // le damos una clase para el CSS
+    monthDays.appendChild(square); //lo añadimos.
   }
 }
-console.log(firstDay);
-console.log(monthLength);
