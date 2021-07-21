c07e2775ae44b38c668edccd039120716b9d7127
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-07-21 16:11:37 +0100

    removeTask() & readTask() added

--- functions.js
+++ functions.js
@@ -1,131 +1,195 @@
 //?----------------------------------------------------- EVENTS ---------------------------------------------------\\
 var btnNewAct = document
-    .getElementById("addActivity")
-    .addEventListener("click", newTask);
+  .getElementById("addActivity")
+  .addEventListener("click", newTask);
 
 var btnCancel = document
-    .getElementById("cancel")
-    .addEventListener("click", cancelTask);
+  .getElementById("cancel")
+  .addEventListener("click", cancelTask);
 
 var closeBtn = document
-    .getElementById("close")
-    .addEventListener("click", cancelTask);
+  .getElementById("close")
+  .addEventListener("click", cancelTask);
 
 var saveBtn = document
-    .getElementById("save")
-    .addEventListener("click", saveTask);
+  .getElementById("save")
+  .addEventListener("click", saveTask);
 
 document.addEventListener("keyup", (e) => {
-    if (e.key == "Escape") {
-        cancelTask();
-    }
+  if (e.key == "Escape") {
+    cancelTask();
+  }
 });
 
 document.addEventListener("click", (e) => {
-    if (e.target === modal) {
-        cancelTask();
-    }
+  if (e.target === modal) {
+    cancelTask();
+  }
 });
 
 //?----------------------------------------------------- FUNCTIONS ---------------------------------------------------\\
 function drawCalendar(firstDay, monthLength) {
-    let monthDays = document.getElementById("days");
+  let monthDays = document.getElementById("days");
 
-    firstDay == 6 && monthLength == 31 ? (n = 42) : (n = 35);
-    firstDay == 7 && monthLength >= 30 ? (n = 42) : (n = 35);
-    firstDay == 1 && monthLength == 28 ? (n = 28) : (n = 35); //! corregir container border
+  firstDay == 6 && monthLength == 31 ? (n = 42) : (n = 35);
+  firstDay == 7 && monthLength >= 30 ? (n = 42) : (n = 35);
+  firstDay == 1 && monthLength == 28 ? (n = 28) : (n = 35); //! corregir container border
 
-    for (let i = 1; i <= n; i++) {
-        var square = document.createElement("div");
-        if (i >= firstDay && i < firstDay + monthLength) {
-            var str = 1 + i - firstDay;
-            str < 10 ? (str = "0" + str) : str;
-            square.setAttribute("id", str);
-            square.innerHTML = 1 + i - firstDay;
-        }
-        square.setAttribute("class", "day-style");
-        monthDays.appendChild(square);
+  for (let i = 1; i <= n; i++) {
+    var square = document.createElement("div");
+    if (i >= firstDay && i < firstDay + monthLength) {
+      var str = 1 + i - firstDay;
+      str < 10 ? (str = "0" + str) : str;
+      square.setAttribute("id", str);
+      square.innerHTML = 1 + i - firstDay;
     }
+    square.setAttribute("class", "day-style");
+    monthDays.appendChild(square);
+  }
 }
 
 function newTask() {
-    modal.classList.replace("display-none", "modal-display-on");
+  modal.classList.replace("display-none", "modal-display-on");
 }
 
 function cancelTask() {
-    for (let i = 0; i < inputs.length; i++) {
-        inputs[i].removeAttribute("required");
-    }
-    modal.classList.replace("modal-display-on", "display-none");
-    document.getElementById("title").value = "";
-    document.getElementById("initial-date").value = "";
-    document.getElementById("final-date").value = "";
-    document.getElementById("exp-time").value = "";
-    document.getElementById("description").value = "";
-    document.getElementById("event-type").value = "";
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].removeAttribute("required");
+  }
+  modal.classList.replace("modal-display-on", "display-none");
+  document.getElementById("title").value = "";
+  document.getElementById("initial-date").value = "";
+  document.getElementById("final-date").value = "";
+  document.getElementById("exp-time").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("event-type").value = "";
 }
 
 function saveTask() {
-    let initialDate = document.getElementById("initial-date").value;
-    let month = initialDate.split("-");
-    let monthStorage = storage.getItem(month[1]);
+  let initialDate = document.getElementById("initial-date").value; //fecha incial input
+  let dateSplit = initialDate.split("-"); //array fecha incial input
+  let monthId = dateSplit[1] + "-" + dateSplit[0]; //creamos el identificado del mes [mes-año]
+  let monthStorage = storage.getItem(monthId); //traemos el JSON [mes-año]
+  let taskStorage = storage.getItem("taskStorage"); //traemos el JSON "taskStorage"
+  let id = storage.getItem("id"); //traemos el JSON "id"
+  let taskId; //inicializamos taskId
 
-    let task = {
-        title: document.getElementById("title").value,
-        initialDate: document.getElementById("initial-date").value,
-        finalDate: document.getElementById("final-date").value, //! GIGANTE (cambio de mes)
-        expTime: document.getElementById("exp-time").value,
-        description: document.getElementById("description").value,
-        type: document.getElementById("event-type").value,
-    };
-    if (monthStorage) {
-        let monthObject = JSON.parse(monthStorage);
-        monthObject.push(task);
-        storage.setItem(month[1], JSON.stringify(monthObject));
-    } else {
-        let monthObject = [];
-        monthObject.push(task);
-        storage.setItem(month[1], JSON.stringify(monthObject));
-    }
-    drawTask();
-    cancelTask();
+  if (id) {
+    // si existe el JSON "id"
+    taskId = parseInt(id) + 1; // taskId = valor numerico de (JSON "id")+1
+    storage.setItem("id", taskId); // sobreescribimos el JSON "id" con el valor de taskId
+  } else {
+    // si NO existe el JSON "id"
+    taskId = 0;
+    storage.setItem("id", 0); // creamos el JSON "id" con el valor 0
+  }
+
+  let task = {
+    //objeto task
+    id: taskId,
+    title: document.getElementById("title").value,
+    initialDate: document.getElementById("initial-date").value,
+    finalDate: document.getElementById("final-date").value,
+    expTime: document.getElementById("exp-time").value,
+    description: document.getElementById("description").value,
+    type: document.getElementById("event-type").value,
+  };
+  let tasksObject = taskStorage ? JSON.parse(taskStorage) : []; // si existe el JSON "taskStorage" el array taskObject es =  "taskStorage" si no crealo
+  tasksObject.push(task); //añadimos al Array tasksObject la nueva task
+  storage.setItem("taskStorage", JSON.stringify(tasksObject)); //creamos o sobreescribimos taskStorage con el valor del array tasksObject
+
+  let idList = monthStorage ? JSON.parse(monthStorage) : []; // si existe el JSON "monthStorage" el array idList es =  "monthStorage" si no crealo
+  idList.push(taskId); //añadimos al Array idList la nueva task
+  storage.setItem(monthId, JSON.stringify(idList)); //creamos o sobreescribimos idList con el valor del array tasksObject
+
+  drawTask();
+  cancelTask();
 }
 
 function drawTask() {
-    let monthToDraw = storage.getItem(mm);
-    monthToDraw = JSON.parse(monthToDraw);
-    if (monthToDraw != null) {
-        let n = 0;
-        for (let x = 0; x < monthToDraw.length; x++) {
-            let initialDay = monthToDraw[x].initialDate;
+  //! se podria eliminar el idsList???
+  let idsList = storage.getItem(mm + "-" + yyyy);
+  idsList = JSON.parse(idsList);
+  let tasksObject = storage.getItem("taskStorage");
+  tasksObject = JSON.parse(tasksObject);
+  if (idsList) {
+    for (const id of idsList) {
+      for (let i = 0; i < tasksObject.length; i++) {
+        if (id == tasksObject[i].id) {
+          let abr = document.getElementById(tasksObject[i].id);
+          if (!abr) {
+            let initialDay = tasksObject[i].initialDate;
             let theinitialDay = initialDay.split("-");
             theinitialDay = theinitialDay[2];
             let toWrite = document.getElementById(theinitialDay);
             let drawEvent = document.createElement("div");
-            drawEvent.innerHTML = monthToDraw[x].title;
+            drawEvent.innerHTML = tasksObject[i].title;
             drawEvent.setAttribute("class", "event-style");
-            drawEvent.setAttribute("id", theinitialDay + "-" + n);
-            n++;
+            drawEvent.setAttribute("id", tasksObject[i].id);
             toWrite.appendChild(drawEvent);
-            drawEvent.addEventListener("click", readTask);
+            drawEvent.addEventListener("click", deleteTask);
+          }
         }
+      }
     }
+  }
 }
 
 function readTask(e) {
-    let selectTask = e.target.innerHTML;
-    let monthToDraw = storage.getItem(mm);
-    monthToDraw = JSON.parse(monthToDraw);
-    for (let i = 0; i < monthToDraw.length; i++) {
-        if (selectTask == monthToDraw[i].title) {
-            modal.classList.replace("display-none", "modal-display-on");
-            document.getElementById("title").value = monthToDraw[i].title;
-            document.getElementById("initial-date").value =
-                monthToDraw[i].initialDate;
-            document.getElementById("final-date").value = monthToDraw[i].finalDate;
-            document.getElementById("exp-time").value = monthToDraw[i].expTime;
-            document.getElementById("description").value = monthToDraw[i].description;
-            document.getElementById("event-type").value = monthToDraw[i].type;
-        }
+  let selectTask = e.target.id;
+  let tasksObject = storage.getItem("taskStorage");
+  tasksObject = JSON.parse(tasksObject);
+
+  for (let i = 0; i < tasksObject.length; i++) {
+    if (selectTask == tasksObject[i].id) {
+      modal.classList.replace("display-none", "modal-display-on");
+      document.getElementById("title").value = tasksObject[i].title;
+      document.getElementById("initial-date").value =
+        tasksObject[i].initialDate;
+      document.getElementById("final-date").value = tasksObject[i].finalDate;
+      document.getElementById("exp-time").value = tasksObject[i].expTime;
+      document.getElementById("description").value = tasksObject[i].description;
+      document.getElementById("event-type").value = tasksObject[i].type;
     }
+  }
+}
+
+function deleteTask(e) {
+  alert("hola");
+  let id = e.target.id;
+  let tasksObject = storage.getItem("taskStorage");
+  tasksObject = JSON.parse(tasksObject);
+  let idsList = storage.getItem(mm + "-" + yyyy);
+  idsList = JSON.parse(idsList);
+  for (let i = 0; i < tasksObject.length; i++) {
+    if (id == tasksObject[i].id) {
+      let deleteIndex = tasksObject.indexOf(tasksObject[i]);
+      tasksObject.splice(deleteIndex, 1);
+      storage.setItem("taskStorage", JSON.stringify(tasksObject));
+      let deleteIdsList = idsList.indexOf(id);
+      idsList.splice(deleteIdsList, 1);
+      storage.setItem(mm + "-" + yyyy, JSON.stringify(idsList));
+      document.getElementById(id).remove();
+    }
+  }
+}
+
+function editTask(e) {
+  //* ORCO poner edit
+  let selectTask = e.target.id;
+  let tasksObject = storage.getItem("taskStorage");
+  tasksObject = JSON.parse(tasksObject);
+
+  for (let i = 0; i < tasksObject.length; i++) {
+    if (selectTask == tasksObject[i].id) {
+      modal.classList.replace("display-none", "modal-display-on");
+      document.getElementById("title").value = tasksObject[i].title;
+      document.getElementById("initial-date").value =
+        tasksObject[i].initialDate;
+      document.getElementById("final-date").value = tasksObject[i].finalDate;
+      document.getElementById("exp-time").value = tasksObject[i].expTime;
+      document.getElementById("description").value = tasksObject[i].description;
+      document.getElementById("event-type").value = tasksObject[i].type;
+    }
+  }
 }
