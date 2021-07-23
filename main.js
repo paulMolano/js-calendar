f2590c50ae285c52cb4a710bd62715413ee9a72a
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-07-23 09:15:59 +0100

    add task button on hover

--- main.js
+++ main.js
@@ -2,3 +2,29 @@
 
 drawCalendar(firstDay, monthLength);
 drawTask();
+
+var dayStyle = document.querySelectorAll(".day-style");
+for (const day of dayStyle) {
+  day.addEventListener("mouseover", hoveringIn);
+  day.addEventListener("mouseout", hoveringOut);
+}
+
+function hoveringIn(event) {
+  var btn;
+  if (event.target.matches(".day-style")) {
+    btn = event.target;
+  }
+
+  var button = document.createElement("button");
+  button.classList.add("add-button");
+  button.setAttribute("id", "button-" + btn.id);
+  button.innerHTML = "+";
+  btn.appendChild(button);
+  btn.classList.toggle("looking");
+}
+
+function hoveringOut(e) {
+  let btn = e.target;
+  btn.classList.toggle("looking");
+  document.getElementById("button-" + btn.id).remove();
+}
